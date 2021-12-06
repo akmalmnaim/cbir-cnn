0d08d4937426f71a9786be57cdc34794f9855c76
akmalmnaim <akmalmnaim12@gmail.com>
2021-12-06 22:26:39 +0800

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,18 @@ from pathlib import Path
 
 app = Flask(__name__)
 
+class FeatureExtractor:
+    def __init__(self):
+        base_model = VGG19(weights='imagenet')
+        self.model = Model(inputs=base_model.input, outputs=base_model.get_layer("fc1").output)
 
+    def extract(self, img):
+        img = img.resize((224,224)).convert("RGB")
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        feature = self.model.predict(x)[0]
+        return feature / np.linalg.norm(feature)
 fe = FeatureExtractor()
 features = []
 img_paths = []
@@ -35,6 +46,16 @@ def index():
         return render_template("index1.html", query_path=uploaded_img_path, scores =scores) 
     else:    
         return render_template("index1.html")
+
+
+
+import tensorflow
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg19 import VGG19, preprocess_input
+from tensorflow.keras.models import Model
+from vit import VisionTransformer
+import numpy as np
+
 
 
 if __name__ == '__main__':
